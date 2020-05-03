b6ae6eb7c043ea4e071da41317c6b3d4c7a744ac
benlac <benoit.lacombled@gmail.com>
2020-05-03 23:42:12 +0200

    Refacto twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,11 @@ class HomeController extends AbstractController
     {
         return $this->render('home/home.html.twig');
     }
+    /**
+     * @Route("/about", name="about", methods={"GET"})
+     */
+    public function about()
+    {
+        return $this->render('home/about.html.twig');
+    }
 }
