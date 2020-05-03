5d0b32915b444af83237b187497a1b909509b8d9
benlac <benoit.lacombled@gmail.com>
2020-05-03 12:33:46 +0200

    Creation of entities : user and role with relation

---
+++ src/Entity/User.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+    
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $idInstagram;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $idFacebook;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $idYoutube;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $idSnapchat;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $idTiktok;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Role")
+     */
+    private $userRoles;
+
+    public function __construct()
+    {
+        $this->userRoles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->userRoles;
+
+        $userRoles = [];
+
+        foreach ($roles as $role) {
+            $userRoles[] = $role->getName();
+        }
+
+        return $userRoles;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getIdInstagram(): ?string
+    {
+        return $this->idInstagram;
+    }
+
+    public function setIdInstagram(?string $idInstagram): self
+    {
+        $this->idInstagram = $idInstagram;
+
+        return $this;
+    }
+
+    public function getIdFacebook(): ?string
+    {
+        return $this->idFacebook;
+    }
+
+    public function setIdFacebook(?string $idFacebook): self
+    {
+        $this->idFacebook = $idFacebook;
+
+        return $this;
+    }
+
+    public function getIdYoutube(): ?string
+    {
+        return $this->idYoutube;
+    }
+
+    public function setIdYoutube(?string $idYoutube): self
+    {
+        $this->idYoutube = $idYoutube;
+
+        return $this;
+    }
+
+    public function getIdSnapchat(): ?string
+    {
+        return $this->idSnapchat;
+    }
+
+    public function setIdSnapchat(?string $idSnapchat): self
+    {
+        $this->idSnapchat = $idSnapchat;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getIdTiktok(): ?string
+    {
+        return $this->idTiktok;
+    }
+
+    public function setIdTiktok(?string $idTiktok): self
+    {
+        $this->idTiktok = $idTiktok;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Role[]
+     */
+    public function getUserRoles(): Collection
+    {
+        return $this->userRoles;
+    }
+
+    public function addUserRole(Role $userRole): self
+    {
+        if (!$this->userRoles->contains($userRole)) {
+            $this->userRoles[] = $userRole;
+        }
+
+        return $this;
+    }
+
+    public function removeUserRole(Role $userRole): self
+    {
+        if ($this->userRoles->contains($userRole)) {
+            $this->userRoles->removeElement($userRole);
+        }
+
+        return $this;
+    }
+}
