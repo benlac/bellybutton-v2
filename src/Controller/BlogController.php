038741de6f8a5353b7ebdb198ad4d399260ece50
benlac <benoit.lacombled@gmail.com>
2020-05-04 15:14:43 +0200

    Management of tag display

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Commentary;
 use App\Repository\ArticleRepository;
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,24 +14,28 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog", methods={"GET"})
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index(ArticleRepository $articleRepository, TagRepository $tagRepository)
     {
         $articles = $articleRepository->findBy([], [
             'createdAt' => 'DESC',
         ]);
+        $tags = $tagRepository->findAll();
         return $this->render('blog/index.html.twig', [
             'articles' => $articles,
+            'tags' => $tags,
         ]);
     }
     /**
      * @Route("/blog/{id}", name="blog_show", methods={"GET"})
      */
-    public function show(Article $article, Commentary $commentary)
+    public function show(Article $article, Commentary $commentary, TagRepository $tagRepository)
     {
+        $tags = $tagRepository->findAll();
 
         return $this->render('blog/article.html.twig', [
             'article' => $article,
-            'comment' => $commentary
+            'comment' => $commentary,
+            'tags' => $tags,
         ]);
     }
 }
