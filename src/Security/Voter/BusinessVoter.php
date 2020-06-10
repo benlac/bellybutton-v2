4b2ff29028ebc112909ba32ce92c8d3bfebb1e0a
benlac <benoit.lacombled@gmail.com>
2020-06-10 16:18:32 +0200

    Voter for business dahsboard

---
+++ src/Security/Voter/BusinessVoter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BusinessVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return in_array($attribute, ['SHOW'])
+            && $subject instanceof User;
+    }
+
+    protected function voteOnAttribute($attribute, $business, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case 'SHOW':
+                return $business === $user;
+                break;
+        }
+
+        return false;
+    }
+}
