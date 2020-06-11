2067aedfc1244a712f6e43d089103e32dc924bfa
benlac <benoit.lacombled@gmail.com>
2020-06-11 10:16:36 +0200

    FIX: bug webpack

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/about/App';
+import App from './components/About/App';
 import '../scss/app.scss';
 
 const target = document.getElementById('root_about');
