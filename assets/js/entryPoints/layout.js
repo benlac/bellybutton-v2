d7b68a12c4a817ad36988b8abddd6b51e815bc7f
benlac <benoit.lacombled@gmail.com>
2020-06-19 16:55:34 +0200

    WIP: Responsive: burger menu

--- assets/js/entryPoints/layout.js
+++ assets/js/entryPoints/layout.js
@@ -1,3 +1,4 @@
+// menu auth desktop
 const loginAuth = document.getElementById('login_auth');
 const menuAuth = document.getElementById('auth_menu');
 
@@ -5,3 +6,14 @@ loginAuth.addEventListener('click', (e) => {
   e.preventDefault();
   menuAuth.classList.toggle('menu__auth--visible');
 })
+
+const iconBurger = document.getElementById('burger__menu__link');
+const burgerMenu = document.getElementById('menu__burger');
+
+console.log(burgerMenu);
+
+iconBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  burgerMenu.classList.toggle('burger__menu--visible');
+})
+
