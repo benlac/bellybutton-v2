190ff065b36fb3ccdefb6f19d9536911c843c2d1
Flxibit <regex@fastmail.com>
2020-09-29 09:22:10 +0200

    Some TODO and try/error on add.html.twig

--- src/Controller/InfluencerManagementController.php
+++ src/Controller/InfluencerManagementController.php
@@ -6,7 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InfluencerManagementController extends AbstractController
-{
+{   //TODO extend the number of routes that connect to this controler
+    //TODO figure out a way to secure the acess to this controler==> Maybe link it the same way as Dashboard
+    //TODO figure out the service runner for TK; IG and TW (YT seems implemented) ==> Maybe implement a meta-runner?
+
     /**
      * @Route("InfluencerManagement/add", name="addInfluencer")
      */
@@ -21,4 +24,11 @@ class InfluencerManagementController extends AbstractController
     {
         return $this->render('influencerManagement/influencer.html.twig');
     }
+    /**
+     * @Route("InfluencerManagement/delete", name="deleteInfluencer")
+     */
+    public function removeInfluencer()
+    {
+        return $this->render('influencerManagement/remove.html.twig');
     }
+}
